05c8a71879355b291eaa5baa3313d08b211b531a
Selena Carlson <selena.h.carlson@gmail.com>
2018-03-04 17:37:12 -0600

    prettier

--- server/public/scripts/controllers/swapi-controller.js
+++ server/public/scripts/controllers/swapi-controller.js
@@ -16,14 +16,12 @@ let self = this;
             url: `https://swapi.co/api/${self.topic}${self.search}${self.keyword}`
         }).then(function(response){
             self.searchResult.list = response.data.results;
-            console.log(self.searchResult.list);
         }).catch(function(error){
             console.log('error on get', error);
         })
     }
 
     self.favoriteResult = function(result){
-        console.log(result);
         $http({
             method: 'POST',
             url: '/favorites',
@@ -59,8 +57,6 @@ let self = this;
             console.log('error on delete', error);
         })
     };
-
-
 
     // let self = this;
 
