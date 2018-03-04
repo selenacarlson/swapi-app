2b624306289564710b8a7a0390a7f765bee1440c
Selena Carlson <selena.h.carlson@gmail.com>
2018-03-03 18:18:08 -0600

    url working without service

--- server/public/scripts/controllers/swapi-controller.js
+++ server/public/scripts/controllers/swapi-controller.js
@@ -1,11 +1,33 @@
-app.controller('StarController', ['StarService', function(StarService){
+app.controller('StarController', ['$http', function($http){
+    
+    // let self = this;
+
+    // self.topic = StarService.topic;
+    // self.search = StarService.search;
+    // self.keyword = StarService.keyword;
+
+    // self.getSwapi = StarService.getSwapi;
+    // self.searchResult = StarService.searchResult;
     
     let self = this;
 
-    self.getSwapi = StarService.getSwapi;
-    self.searchResult = StarService.searchResult;
+    self.searchResult = { list: [] };
 
-    self.getSwapi();
+    self.topic = '';
+    self.search = '/?search=';
+    self.keyword = '';
 
+    self.getSwapi = function(){
+        let url = `https://swapi.co/api/${self.topic}${self.search}${self.keyword}`;
+        $http({
+            method: 'GET',
+            url: `https://swapi.co/api/${self.topic}${self.search}${self.keyword}`
+        }).then(function(response){
+            self.searchResult.list = response.data.results;
+            console.log(self.searchResult.list);
+        }).catch(function(error){
+            console.log('error on get', error);
+        })
+    }
 
 }])
--- server/public/scripts/services/swapi-service.js
+++ server/public/scripts/services/swapi-service.js
@@ -4,15 +4,21 @@ app.service('StarService', ['$http', function($http){
 
     self.searchResult = { list: [] };
 
+    self.topic = '';
+    self.search = '/?search=';
+    self.keyword = '';
+
     self.getSwapi = function(){
+        let url = `https://swapi.co/api/${self.topic}${self.search}${self.keyword}`;
         $http({
             method: 'GET',
-            url: 'https://swapi.co/api/people/1/'
+            url: `https://swapi.co/api/${self.topic}${self.search}${self.keyword}`
         }).then(function(response){
             console.log(response);
             self.searchResult.list = response.data;
         }).catch(function(error){
             console.log(error);
+            console.log(url);
         })
     }
 
